b43e19bb93e14dccb33edb66c820af21fce228a3
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-03-17 19:37:45 +0000

    set up url links for 'articles:list' & 'articles:detail'

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
 
@@ -5,3 +6,5 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', { 'articles': articles })
    
+def article_detail(request, slug):
+    return HttpResponse(slug)
