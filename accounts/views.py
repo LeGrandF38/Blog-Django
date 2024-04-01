923404f25bca702969037ef77e81f75f53f15c50
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-04-01 14:23:43 +0000

    add login & logout logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib.auth import login, logout
 
 
 def signup_view(request):
     if request.method == 'POST':
          form = UserCreationForm(request.POST)
          if form.is_valid():
-             form.save()
+             user = form.save()
              #  log the user in
+             login(request, user)
              return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -19,8 +20,16 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # log the user in
+           # log the user in
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', { 'form': form })
+
+
+def logout_view(request):
+    if request.method == 'POST':
+            logout(request)
+            return redirect('articles:list')
